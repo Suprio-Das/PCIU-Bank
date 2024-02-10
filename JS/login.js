a39d0969cc61d5005971c56afd0cd5fde8fc7698
Suprio-Das <priyamd507.gmail.com>
2024-02-11 01:52:07 +0600

    index.html file completed and bank.html is ready to go

---
+++ JS/login.js
@@ -0,0 +1,20 @@
+document.getElementById('btn-submit').addEventListener('click', function(){
+    
+    // getting email from email field
+    const emailField = document.getElementById('user-email');
+    const emailFieldText = emailField.value;
+
+    // getting password from password field
+    const passwordField = document.getElementById('user-password');
+    const passwordFieldText = passwordField.value;
+
+    // validating user
+    if(emailFieldText === 'goriberbank@pciu.com' && passwordFieldText === 'password'){
+        // Redirect to "bank.html" when the credentials are correct
+        document.location.href = 'bank.html',true;
+
+    } else {
+        // Display an alert or perform other actions for invalid credentials
+        alert('Invalid credentials. Please try again.');
+    }
+})
